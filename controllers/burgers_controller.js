8ab16c1353f65c3e61b3da3d19e9bcf2df6e9754
JonM <jonmisner86@gmail.com>
2020-10-03 16:39:13 -0500

    haldlebars updates

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const burger = require("../models/burger.js");
+const router = espress.Router();
 
 // create router for the app
+router.get("/", function(req, res){
+   burger.all(function(data){
+      let hbsObject = {
+         cats: data
+      };
+      console.log(hbsObject);
+      res.render("index", hbsObject);
+   });
+});
+router.post("/api/burgers", functin (req, res) {
+   burger.create(["burger_name", "devoured"])
+})
 
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,5 +1,25 @@
 const orm = require("../config/orm.js")
 
 // call orm functions using burger specific input
+const burger = {
+   selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+         cb(res);
+      });
+   },
+
+   insertOne: function(cols, vals, cb) {
+      orm.insertOne("burgers", cols, vals, function(res){
+         cb(res);
+      });
+   },
+
+   updateOne: function(objColVals, condition, cb) {
+      orm.updateOne("burgers", objColVals, condition, function(res){
+         cb(res);
+      });
+   }
+
+}
 
 module.exports = burger;
